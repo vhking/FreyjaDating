1d03104f54c9452622c096f4ee49cd93f824a84c
vhking <vidarheltne@hotmail.no>
2018-03-27 11:06:48 +0200

    Migration , database

--- FreyjaDating.API/Controllers/ValuesController.cs
+++ FreyjaDating.API/Controllers/ValuesController.cs
@@ -2,25 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreyjaDating.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FreyjaDating.API.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+            
+        }
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetValues(int id)
         {
-           return "value";           
+           var value = await _context.Values.FirstOrDefaultAsync(x=> x.Id == id);         
+
+           return Ok(value);  
         }
 
         // POST api/values
