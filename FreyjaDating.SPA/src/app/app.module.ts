eaf55184260ea88ce633f6fb7114d11893fcbf5e
vhking <vidarheltne@hotmail.no>
2018-03-28 11:46:13 +0200

    Home and register components

--- FreyjaDating.SPA/src/app/app.module.ts
+++ FreyjaDating.SPA/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { AuthService } from './_services/auth.service';
   declarations: [
     AppComponent,
     ValueComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
 ],
   imports: [
     BrowserModule,
